a61ea2b56e5882f905c54eae39a45d51d1454766
wupengwei <wpw0704@163.com>
2026-07-14 16:08:23 +0800

    持续进行OP

--- common/renesas/module/gpt/gpt.h
+++ common/renesas/module/gpt/gpt.h
@@ -5,7 +5,16 @@
 #ifndef RZN2L_LWIP_ETHERCAT_MASTER_REV0300_GPT_H
 #define RZN2L_LWIP_ETHERCAT_MASTER_REV0300_GPT_H
 
-#include "main_thread.h"
+#include "hal_data.h"
+#include "FreeRTOS.h"
+#include "task.h"
 #include "um_common.h"
+
+fsp_err_t gpt_init(void);
+fsp_err_t gpt_stop(void);
+
+void gpt_set_notify_task(TaskHandle_t task_handle);
+
+// void gpt0_callback(timer_callback_args_t *p_args);
 
 #endif //RZN2L_LWIP_ETHERCAT_MASTER_REV0300_GPT_H
